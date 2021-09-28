e1ec83a2739cff732c0d82d1b6d02c8c41dcf665
Hendrik Sebastian Voß <hendrik@familievoss.org>
2021-09-28 16:14:31 +0200

    Initial commit

---
+++ src/main/java/de/hsvoss/vision_tweet_ng_backend/VisionTweetNgBackendApplication.java
@@ -0,0 +1,13 @@
+package de.hsvoss.vision_tweet_ng_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionTweetNgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionTweetNgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsvoss/vision_tweet_ng_backend/VisionTweetNgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsvoss.vision_tweet_ng_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionTweetNgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
