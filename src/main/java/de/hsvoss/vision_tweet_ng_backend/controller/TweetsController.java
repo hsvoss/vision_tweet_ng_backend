eb408a17979a96f5818f12425570048946199988
Hendrik Sebastian Voß <hendrik@familievoss.org>
2021-09-28 17:34:58 +0200

    Backend durch implementiert, mit ein paar Bugs

--- src/main/java/de/hsvoss/vision_tweet_ng_backend/controller/TweetsController.java
+++ src/main/java/de/hsvoss/vision_tweet_ng_backend/controller/TweetsController.java
@@ -4,7 +4,8 @@ import de.hsvoss.vision_tweet_ng_backend.apimodel.TweetRequestDTO;
 import de.hsvoss.vision_tweet_ng_backend.apimodel.TweetResponseDTO;
 import de.hsvoss.vision_tweet_ng_backend.domain.Tweet;
 import de.hsvoss.vision_tweet_ng_backend.services.TweetService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,7 +21,7 @@ public class TweetsController {
 //    https://vision-tweet-ng-backend.herokuapp.com/
 //    https://docs.microsoft.com/de-de/azure/architecture/best-practices/api-design
 
-//    @Autowired
+    //    @Autowired
     ConversionService conversionService;
 
     TweetService tweetService;
@@ -48,13 +50,21 @@ public class TweetsController {
         return convert;
     }
 
-    @GetMapping(path = "tweets/{id}")
-    TweetResponseDTO getOneTweet(@PathParam("id") UUID id) {
+    @GetMapping("tweets/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "the tweet"),
+            @ApiResponse(responseCode = "404", description = "no tweet for the id")
+    })
+    TweetResponseDTO getTweet(@PathVariable("id") UUID id) {
+        Optional<Tweet> tweet = tweetService.getTweet(id);
+        if (!tweet.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return conversionService.convert(tweet, TweetResponseDTO.class);
+        }
 
-//        TweetResponseDTO tweetResponseDTO = new TweetResponseDTO();
-//        tweetResponseDTO.setMessage("Hallo Welt 2");
-        return null;
     }
+
 
     @PostMapping(path = "tweets")
     TweetResponseDTO createTweet(@RequestBody TweetRequestDTO request) {
@@ -67,13 +77,16 @@ public class TweetsController {
 
     @DeleteMapping(path = "tweets/{id}")
     void deleteOneTweet(@PathParam("id") UUID id) {
-
+        tweetService.deleteTweet(id);
     }
 
     @PutMapping(path = "tweets/{id}")
-    TweetResponseDTO createTweet(@PathParam("id") UUID id,
+    TweetResponseDTO updateTweet(@PathParam("id") UUID id,
                                  @RequestBody TweetRequestDTO request) {
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        Tweet mappedRequest = conversionService.convert(request, Tweet.class);
+
+        Tweet erstellteTweet = tweetService.updateTweet(mappedRequest);
+        return conversionService.convert(erstellteTweet, TweetResponseDTO.class);
     }
 
 
