9bd356e74d685deb2b4af2549694b4cd3a16d95a
Hendrik Sebastian Voß <hendrik@familievoss.org>
2021-09-28 18:08:53 +0200

    Bugfix

--- src/main/java/de/hsvoss/vision_tweet_ng_backend/controller/TweetsController.java
+++ src/main/java/de/hsvoss/vision_tweet_ng_backend/controller/TweetsController.java
@@ -11,9 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -44,9 +42,7 @@ public class TweetsController {
     List<TweetResponseDTO> getAllTweets() {
 
         List<Tweet> allTweets = tweetService.getAllTweets();
-        List<TweetResponseDTO> convert = conversionService.convert(allTweets, List.class);
-
-        return convert;
+        return conversionService.convert(allTweets, List.class);
     }
 
     @GetMapping("tweets/{id}")
@@ -55,13 +51,9 @@ public class TweetsController {
             @ApiResponse(responseCode = "404", description = "no tweet for the id")
     })
     TweetResponseDTO getTweet(@PathVariable("id") UUID id) {
-        Optional<Tweet> tweet = tweetService.getTweet(id);
-        if (!tweet.isPresent()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        } else {
-            return conversionService.convert(tweet, TweetResponseDTO.class);
-        }
-
+        Tweet tweet = tweetService.getTweet(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return conversionService.convert(tweet, TweetResponseDTO.class);
     }
 
 
@@ -83,12 +75,13 @@ public class TweetsController {
             @ApiResponse(responseCode = "204", description = "tweet deleted"),
             @ApiResponse(responseCode = "404", description = "no tweet for the id")
     })
-    void deleteOneTweet(@PathParam("id") UUID id) {
+    void deleteOneTweet(@PathVariable("id") UUID id) {
         tweetService.deleteTweet(id);
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping(path = "tweets/{id}")
-    TweetResponseDTO updateTweet(@PathParam("id") UUID id,
+    TweetResponseDTO updateTweet(@PathVariable("id") UUID id,
                                  @RequestBody TweetRequestDTO request) {
         Tweet mappedRequest = conversionService.convert(request, Tweet.class);
 
